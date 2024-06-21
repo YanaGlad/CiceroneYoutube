52da06af447fc307bbf45345147f61bb94dc2284
y.gladkikh <y.gladkikh@tinkoff.ru>
2024-06-21 11:26:17 +0300

    fix blank fragment screen

--- app/src/main/java/com/example/demodaggercicerone/MainActivity.kt
+++ app/src/main/java/com/example/demodaggercicerone/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.demodaggercicerone.cicerone.Router
 import com.example.demodaggercicerone.cicerone.ScreenProvider
-import com.example.demodaggercicerone.screens.BlankFragmentScreen
+import com.example.demodaggercicerone.screens.BlankFragmScreen
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +13,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val router = Router(ScreenProvider(SCREEN_PROVIDERS), this)
-        router.navigateTo(BlankFragmentScreen)
+        router.navigateTo(BlankFragmScreen)
     }
 }
